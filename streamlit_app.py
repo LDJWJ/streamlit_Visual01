51f2d1b041cd08ec418c7671edc36dffac4ed2da
LDJWJ <frontierall@naver.com>
2024-01-17 16:26:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,11 +10,11 @@ DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
 	
 @st.cache
 def load_data(nrows):
-      data = pd.read_csv(DATA_URL, nrows=nrows)
-      lowercase = lambda x: str(x).lower()
-      data.rename(lowercase, axis='columns', inplace=True)
-      data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
-      return data
+    data = pd.read_csv(DATA_URL, nrows=nrows)
+    lowercase = lambda x: str(x).lower()
+    data.rename(lowercase, axis='columns', inplace=True)
+    data[DATE_COLUMN] = pd.to_datetime(data[DATE_COLUMN])
+    return data
 	
 data_load_state = st.text('Loading data...')
 data = load_data(10000)
@@ -31,5 +31,5 @@ st.bar_chart(hist_values)
 hour_to_filter = st.slider('hour', 0, 23, 17)
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
 	
-  st.subheader('Map of all pickups at %s:00' % hour_to_filter)
-  st.map(filtered_data)
+st.subheader('Map of all pickups at %s:00' % hour_to_filter)
+st.map(filtered_data)
